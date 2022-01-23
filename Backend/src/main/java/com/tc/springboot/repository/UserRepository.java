6b76275f28321e471fdeca0627d296c7da1cd040
tekies0909@gmail.com <tekies0909@gmail.com>
2022-01-23 14:01:36 +0900

    Feat: add Backend UserManage add User Join add User Update add User Login(No jwt)

--- Backend/src/main/java/com/tc/springboot/repository/UserRepository.java
+++ Backend/src/main/java/com/tc/springboot/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.tc.springboot.repository;
 
 import com.tc.springboot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select u from tc_user u where u.tc_id = :tc_id")
+    User findUserBytc_id(String tc_id);
+
+    @Query("select tc_password from tc_user where tc_id = :tc_id")
+    User findtc_passwordBytc_id(String tc_id);
 }
